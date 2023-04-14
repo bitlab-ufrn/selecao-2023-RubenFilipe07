65f6ebb91c63b627592fddc40bd24275ec7be486
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-14 18:21:51 -0300

    reafactor(HomeContent): change content and link

--- detector-ofensas/src/components/HomeContent/HomeContent.jsx
+++ detector-ofensas/src/components/HomeContent/HomeContent.jsx
@@ -13,11 +13,11 @@ const HomeContent = () => (
             <h1>Bem-vindo ao nosso detector de ofensas!</h1>
             <p>Nosso objetivo é tornar a internet um lugar mais seguro e saudável para todos. Por isso, criamos um sistema de detecção de palavrões e ofensas que bloqueia automaticamente comentários indevidos em suas postagens.</p>
             <p>Com nosso detector de ofensas dinâmico, você pode personalizar a lista de palavras a serem detectadas de acordo com as necessidades do seu negócio.</p>
-            <img src={demo_remover} alt="Demo remover ofensa" />
-            <p>Além disso, nossa ferramenta é capaz de detectar palavras com acentos e/ou caracteres especiais que simulam uma letra, garantindo que nenhum comentário inapropriado passe despercebido.</p>
-            <img src={demo_caracteres} alt="Demo caracteres especiais" />
+            <img src={demo_remover} alt="Demonstração da funcionalidade de remover ofensa" />
+            <p>Além disso, nossa ferramenta é capaz de detectar palavras com ou sem acentos e/ou caracteres especiais que simulam uma letra, garantindo que nenhum comentário inapropriado passe despercebido.</p>
+            <img src={demo_caracteres} alt="Demotração da funcionalidade de detectar caracteres especiais" />
             <p>Experimente agora mesmo nosso detector de ofensas dinâmico e comece a criar um ambiente online mais positivo e respeitoso.</p>
-            <Link to={'/gerenciar-ofensas'}>
+            <Link to={'/detector'}>
                 <Button type="primary" size="large">Experimente agora</Button>
             </Link>
 
