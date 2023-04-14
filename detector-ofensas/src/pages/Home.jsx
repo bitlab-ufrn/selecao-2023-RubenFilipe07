c1d3438762f88feb5650b5e65fcfd255f0f6b9dc
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-14 18:29:55 -0300

    refactor(home): Change menu data

--- detector-ofensas/src/pages/Home.jsx
+++ detector-ofensas/src/pages/Home.jsx
@@ -1,42 +1,70 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 
 import {
     Menu,
-    Layout
+    Layout,
+    Button
 } from 'antd'
 
 import FooterContent from '../components/FooterContent/FooterContent';
 import HomeContent from '../components/HomeContent/HomeContent';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/authContext';
 
 const { Footer, Content } = Layout;
 
-import { HomeOutlined, MenuOutlined, LoginOutlined, UnorderedListOutlined } from '@ant-design/icons';
-
-const itensMenu = [
-    {
-        key: '1',
-        icon: <HomeOutlined />,
-        label: (<Link to="/">Início</Link>),
-    },
-    {
-        key: '2',
-        icon: <UnorderedListOutlined />,
-        label: (<Link to="/detector">Detectar Linguagem Imprópria</Link>),
-    },
-    {
-        key: '3',
-        icon: <LoginOutlined />,
-        label: (<Link to="/login">Login do Administrador</Link>),
-    }
-
-]
+import { HomeOutlined, MenuOutlined, LoginOutlined, UnorderedListOutlined, LogoutOutlined } from '@ant-design/icons';
 
 
-const Home = () => (
+
+const Home = () => {
+    const {isAuth, handleLogout} =  useContext(AuthContext);
+
+    const itensMenuNoAuth = [
+        {
+            key: '1',
+            icon: <HomeOutlined />,
+            label: (<Link to="/">Início</Link>),
+        },
+        {
+            key: '2',
+            icon: <UnorderedListOutlined />,
+            label: (<Link to="/detector">Detectar Linguagem Imprópria</Link>),
+        },
+        {
+            key: '3',
+            icon: <LoginOutlined />,
+            label: (<Link to="/login">Login do Administrador</Link>),
+        }
+    
+    ]
+    
+    const itensMenuAuth = [
+        {
+            key: '1',
+            icon: <HomeOutlined />,
+            label: (<Link to="/">Início</Link>),
+        },
+        {
+            key: '2',
+            icon: <UnorderedListOutlined />,
+            label: (<Link to="/detector">Detectar Linguagem Imprópria</Link>),
+        },
+        {
+            key: '3',
+            icon: <MenuOutlined />,
+            label: (<Link to="/gerenciar-ofensas">Gerenciar palavras</Link>),
+        },
+        {
+            key: '4',
+            label: (<Button style={{backgroundColor: "#e52222", color: "white", borderColor: "#e52222"}}  onClick={()=>{handleLogout()}} icon={<LogoutOutlined/>} >Sair</Button>),
+        }
+    ]
+
+    return(
     <Fragment>
         <Layout>
-            <Menu mode="horizontal" defaultSelectedKeys={['1']} items={itensMenu} theme='dark' />
+            {isAuth ? <Menu mode="horizontal" defaultSelectedKeys={['1']} items={itensMenuAuth} theme='dark' /> : <Menu mode="horizontal" defaultSelectedKeys={['1']} items={itensMenuNoAuth} theme='dark' />}        
             <Content style={{ backgroundColor: "#FFFF" }}>
                 <HomeContent />
             </Content>
@@ -46,7 +74,8 @@ const Home = () => (
             </Footer>
         </Layout>
     </Fragment>
+    )
 
-);
+};
 
 export default Home;
