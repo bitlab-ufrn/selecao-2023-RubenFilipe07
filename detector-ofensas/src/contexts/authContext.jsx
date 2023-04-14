42b66f8339cf0c9aeb83140e60282c3df1b536d5
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-14 18:25:41 -0300

    feat(authContext): add keep login functionality

--- detector-ofensas/src/contexts/authContext.jsx
+++ detector-ofensas/src/contexts/authContext.jsx
@@ -1,13 +1,30 @@
-import { createContext, useState } from 'react';
-
+import { createContext, useState, useEffect } from 'react';
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(false);
   const [token, setToken] = useState('');
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      setToken(token);
+      setIsAuth(true);
+    }
+  }, []);
+
+
+  const handleLogout = () => {
+    setIsAuth(false);
+    setToken('');
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('token');
+    window.location = '/';
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuth, setIsAuth, token, setToken }}>
+    <AuthContext.Provider value={{ isAuth, setIsAuth, token, setToken, handleLogout }}>
       {children}
     </AuthContext.Provider>
   );
