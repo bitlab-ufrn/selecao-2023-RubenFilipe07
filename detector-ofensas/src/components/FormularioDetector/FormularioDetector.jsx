35c35b02ec43f6913ec3a742dfb26e3fae2cfc28
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-14 20:05:26 -0300

    feat(FormularioDetector): implement detection

--- detector-ofensas/src/components/FormularioDetector/FormularioDetector.jsx
+++ detector-ofensas/src/components/FormularioDetector/FormularioDetector.jsx
@@ -1,24 +1,99 @@
 import './FormularioDetector.css';
-import { Form, Input, Button } from 'antd';
-
+import { Fragment, useEffect, useState } from 'react';
+import { Form, Input, Button, message, Table, Modal, Card } from 'antd';
+import axios from 'axios';
 
 const FormularioLogin = () => {
+
+    const [resultadoApi, setResultadoApi] = useState([
+        {
+            qtdPalavrasDetectadas: 0,
+            qtdPalavrasCadastradas: 0,
+            palavrasDetectadas: [],
+            porcetagemDetecadasTexto: 0,
+            textoCensurado: '',
+        }
+    ]);
+    const [visible, setVisible] = useState(false);
+
+    const onFinish = async ({ texto }) => {
+        const response = await axios.post('http://localhost:8080/verifica-texto', { texto }, { headers: { 'api-key': 'JhQwXXztY1s5OsSKgj3mMoJ', } });
+        setResultadoApi(response.data);
+    };
+
+   useEffect(() => {
+        if (resultadoApi.qtdPalavrasDetectadas > 0) {
+            setVisible(true);
+            console.log(resultadoApi);
+        }
+    }, [resultadoApi]);
+
     return (
+        <Fragment>
 
-        <Form className='form-detector'>
+            <Form className='form-detector' onFinish={onFinish}>
 
-            <h1 className='titulo-form'>Detector de Ofensas</h1>
+                <h1 className='titulo-form'>Detector de Ofensas</h1>
 
-            <Form.Item className='textarea-detector' label="Texto" name="texto" rules={[{ required: true, message: 'Insira o texto' }]}>
-                <Input.TextArea rows={10} />
-            </Form.Item>
+                <Form.Item className='textarea-detector' label="Texto" name="texto" rules={[{ required: true, message: 'Insira o texto' }]}>
+                    <Input.TextArea rows={10} />
+                </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Detectar
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Detectar
+                    </Button>
+                </Form.Item>
+       
+                    <Fragment>
+                        <Modal
+                            title="Resultado da detecção"
+                            open={visible}
+                            footer={null}
+                            onCancel={() => setVisible(false)}
+                        >
+
+                            <Card title="Palavras detectadas no texto" >
+                                {
+                                    resultadoApi.qtdPalavrasDetectadas > 0 ?
+                                    <p>Palavras detectadas: {resultadoApi.palavrasDetectadas.map(
+                                        (palavra, index) => {
+                                            return (
+                                                <span key={index}>"{palavra}" </span>
+                                            )
+                                        }
+                                    ) }</p> : <></>
+
+                                }
+                                
+                                <p>Quantidade: {resultadoApi.qtdPalavrasDetectadas}</p>
+                            </Card>
+
+                            <Card title="Total de palavras cadastradas">
+                                <p>Palavras cadastradas: {resultadoApi.qtdPalavrasCadastradas}</p>
+                            </Card>
+
+                            <Card title="Texto Censurado">
+                                <p>{resultadoApi.textoCensurado}</p>
+                            </Card>
+
+                            <Card title="Porcentagem impróprias">
+                                <p>{resultadoApi.porcetagemDetecadasTexto.toFixed(2)}% das palavras do texto são impróprias</p>
+                            </Card>
+                   </Modal>
+                    </Fragment>
+
+                
+              
+
+            </Form>
+
+
+
+
+
+
+        </Fragment>
     )
 
 
