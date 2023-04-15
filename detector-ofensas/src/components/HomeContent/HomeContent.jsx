32252bb0e6ccc0c0526f99ad307319c112b70e88
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-14 23:27:43 -0300

    feat(homeContent): add auth based content

--- detector-ofensas/src/components/HomeContent/HomeContent.jsx
+++ detector-ofensas/src/components/HomeContent/HomeContent.jsx
@@ -6,24 +6,52 @@ import logo from '../../assets/logo.png';
 import demo_caracteres from '../../assets/demo_caracteres.gif';
 import demo_remover from '../../assets/demo_remover.gif';
 
-const HomeContent = () => (
-    <div className='inicio'>
-        <div className='apresentacao'>
-            <img src={logo} alt="Logo" />
-            <h1>Bem-vindo ao nosso detector de ofensas!</h1>
-            <p>Nosso objetivo é tornar a internet um lugar mais seguro e saudável para todos. Por isso, criamos um sistema de detecção de palavrões e ofensas que bloqueia automaticamente comentários indevidos em suas postagens.</p>
-            <p>Com nosso detector de ofensas dinâmico, você pode personalizar a lista de palavras a serem detectadas de acordo com as necessidades do seu negócio.</p>
-            <img src={demo_remover} alt="Demonstração da funcionalidade de remover ofensa" />
-            <p>Além disso, nossa ferramenta é capaz de detectar palavras com ou sem acentos e/ou caracteres especiais que simulam uma letra, garantindo que nenhum comentário inapropriado passe despercebido.</p>
-            <img src={demo_caracteres} alt="Demotração da funcionalidade de detectar caracteres especiais" />
-            <p>Experimente agora mesmo nosso detector de ofensas dinâmico e comece a criar um ambiente online mais positivo e respeitoso.</p>
-            <Link to={'/detector'}>
-                <Button type="primary" size="large">Experimente agora</Button>
-            </Link>
+import { AuthContext } from '../../contexts/authContext';
+import { useContext } from 'react';
 
+const HomeContent = () => {
+
+    const { isAuth } = useContext(AuthContext);
+
+    return (
+
+        <div className='inicio'>
+            <div className='apresentacao'>
+                <img src={logo} alt="Logo" />
+                <h1>Bem-vindo ao nosso detector de ofensas!</h1>
+                <p>Nosso objetivo é tornar a internet um lugar mais seguro e saudável para todos. Por isso, criamos um sistema de detecção de palavrões e ofensas que bloqueia automaticamente comentários indevidos em suas postagens.</p>
+                <p>Com nosso detector de ofensas dinâmico, você pode personalizar a lista de palavras a serem detectadas de acordo com as necessidades do seu negócio.</p>
+
+                {isAuth && (
+                    <div className='botoes-home'>
+                        <Link to={'/detector'}>
+                            <Button type="primary" size="large">Detector de Ofensas</Button>
+                        </Link>
+                    
+
+                        <Link to={'/gerenciar-ofensas'}>
+                            <Button type="primary" size="large">Gerenciar Ofensas</Button>
+                        </Link>
+                    </div>
+                )}
+
+                {!isAuth && (
+                    <>
+                        <img src={demo_remover} alt="Demonstração da funcionalidade de remover ofensa" />
+                        <p>Além disso, nossa ferramenta é capaz de detectar palavras com ou sem acentos e/ou caracteres especiais que simulam uma letra, garantindo que nenhum comentário inapropriado passe despercebido.</p>
+                        <img src={demo_caracteres} alt="Demotração da funcionalidade de detectar caracteres especiais" />
+                        <p>Experimente agora mesmo nosso detector de ofensas dinâmico e comece a criar um ambiente online mais positivo e respeitoso.</p>
+                        <Link to={'/detector'}>
+                            <Button type="primary" size="large">Experimente agora</Button>
+                        </Link>
+                    </>
+                )
+                }
+            </div>
         </div>
-    </div>
 
-);
+    )
+
+};
 
 export default HomeContent;
